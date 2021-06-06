02d3d6f675b9d8d430cbcc61c6cce5d9d30a5949
MartinW <wei21921@spengergasse.at>
2021-06-07 00:03:33 +0200

    Weikmann: CreateTable Customer plus rights plus Mockdata

---
+++ Procedure_CT_Customer.sql
@@ -0,0 +1,45 @@
+-- CREATE TABLE, Abgabe WEIKMANN, 6ACIF
+CREATE OR REPLACE PROCEDURE PROC_CT_CUSTOMER IS
+    V_CNT Number;
+BEGIN
+
+    -- set serveroutput on;
+    -- execute dbms_output.put_line ('Versuche eine Tabelle Customer ueber PL/SQL zu erstellen!');
+    
+    SELECT COUNT(*) INTO V_CNT
+       FROM ALL_TABLES
+            WHERE OWNER = 'DEMO' AND
+                  TABLE_NAME = 'CUSTOMER';
+
+      -- execute dbms_output.put_line ('Anzahl der gefundenen CUSTOMER-Tabellen: ' || V_CNT);
+      IF V_CNT = 0 THEN
+         EXECUTE IMMEDIATE 'CREATE TABLE CUSTOMER
+                            (
+                            CUSTOMER_ID INTEGER PRIMARY KEY,
+                            EMAIL       VARCHAR2(45),
+                            FIRSTNAME   VARCHAR2(40),
+                            LASTNAME    VARCHAR2(40),
+                            GENDER      VARCHAR2(20),
+                            BIRTHDAY    DATE,
+                            CREDITS     DECIMAL(4)
+                            )';
+         COMMIT;
+      END IF;
+
+      SELECT COUNT(*) INTO V_CNT
+         FROM ALL_SEQUENCES
+         WHERE SEQUENCE_OWNER = 'DEMO' AND
+         SEQUENCE_NAME = 'SEQ_CUSTOMER_ID';
+
+             
+    IF V_CNT = 0 THEN
+       EXECUTE IMMEDIATE 'CREATE SEQUENCE SEQ_CUSTOMER_ID
+                          START WITH   10000
+                          INCREMENT BY 1
+                          CACHE        50';
+       COMMIT;
+    END IF;
+
+END PROC_CT_CUSTOMER;
+
+
